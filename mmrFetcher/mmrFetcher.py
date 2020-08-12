57911bb1b4a0d198fcb48d78023defe04022401f
aidant19 <aidant19@gmail.com>
2020-08-12 17:37:33 -0400

    Bug fix part 7

--- mmrFetcher/mmrFetcher.py
+++ mmrFetcher/mmrFetcher.py
@@ -27,33 +27,34 @@ class MMRFetcher(commands.Cog):
     @checks.is_owner()
     async def fetch(self, ctx):
         await ctx.send("Fetching MMR data...")
-        with self._createcsv() as w:
+        w = self._createcsv()
 
-            names, links = (["aiTan"], ["https://rocketleague.tracker.network/profile/steam/aidant19"])
-            total = len(names)
-            tenPercent = total / 10
+        names, links = (["aiTan"], ["https://rocketleague.tracker.network/profile/steam/aidant19"])
+        total = len(names)
+        tenPercent = total / 10
 
-            i = 0 #count of each row in the Tracker Links
-            for i in range(0, total):
-                try:
-                    name,link = names[i], links[i]
-                    linksplit = link.split('profile/')
-                    unpack = [ x for x in linksplit[1].split('/') if x]
-                    if "mmr" in unpack:
-                        mmr,platform,gamertag = unpack
-                    else:
-                        platform,gamertag = unpack
-                    await ctx.send(gamertag + " " + platform)
-                    data = self._rlscrape(gamertag,platform)
-                    self._writefetch(w, data, name, link)
-                    i += 1
-                    if i % tenPercent == 0:
-                        await ctx.send("Fetch Progress: {}0% Complete".format(i / tenPercent))
-                except Exception as e:
-                    i += 1
-                    await ctx.send("Error on line {0}: {1}".format(i, e))
-                await asyncio.sleep(.01)
+        i = 0 #count of each row in the Tracker Links
+        for i in range(0, total):
+            try:
+                name,link = names[i], links[i]
+                linksplit = link.split('profile/')
+                unpack = [ x for x in linksplit[1].split('/') if x]
+                if "mmr" in unpack:
+                    mmr,platform,gamertag = unpack
+                else:
+                    platform,gamertag = unpack
+                await ctx.send(gamertag + " " + platform)
+                data = self._rlscrape(gamertag,platform)
+                self._writefetch(w, data, name, link)
+                i += 1
+                if i % tenPercent == 0:
+                    await ctx.send("Fetch Progress: {}0% Complete".format(i / tenPercent))
+            except Exception as e:
+                i += 1
+                await ctx.send("Error on line {0}: {1}".format(i, e))
+            await asyncio.sleep(.01)
 
+        w.close()
         await ctx.send("Done", file=File(Outputcsv))
         os.remove(Outputcsv)
 
